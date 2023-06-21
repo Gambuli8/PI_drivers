9f1f4e3156d441b36ec6a842ae22622d2a49d058
GeroGambuli <gerogambuli2002@gmail.com>
2023-06-20 22:05:50 -0300

    modificando errores

--- client/src/Views/Detail/detail.jsx
+++ client/src/Views/Detail/detail.jsx
@@ -19,23 +19,19 @@ function Detail() {
       {console.log(drivers)}
       {drivers?.Nombre ? (
         <>
-          <Link to='/home'><button className={style.button}>Back</button></Link>
+          {/* <Link to='/home'><button className={style.button}>Back</button></Link> */}
         <div className={style.container}>
-          {/* <div className={style.Card}> */}
             <div className={style.container_info}>
                 <h1 className={style.title}><p>Nombre:</p>{drivers?.Nombre}</h1> 
                 <h3 className={style.info}><p>Id:</p>{drivers?.id}</h3>
                 <h3 className={style.info}><p>Apellido:</p>{drivers?.Apellido}</h3>
                 <h3 className={style.info}><p>Nacionalidad:</p>{drivers?.Nacionalidad}</h3>
                 <h3 className={style.info}> <p>Fecha de nacimiento:</p>{drivers?.Fecha_de_Nacimiento}</h3>
-                <h3 className={style.info}><p>Escuderias:</p> {drivers.Escuderias}</h3>
+                <h3 className={style.info}><p>Escuderias:</p>{drivers?.Escuderias}</h3>
                 <h3 className={style.info}> <p>Descripcion:</p>{drivers?.Descripcion}</h3>
                 <img className={style.image} src={drivers?.Imagen} alt="Imagen no disponible" />
             </div>
-              {/* <div className={style.container_image}> */}
-              {/* </div> */}
           </div>
-        {/* </div> */}
         </>
       ) : (
         <h3 className={style.loading}>Loading...</h3>
